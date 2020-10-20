cc56549ad9dbe398735930fd75adca178018efbe
klenwell <klenwell@gmail.com>
2020-10-19 19:02:06 -0700

    Updates new OC Daily Testing Analytics class to collect csv data.

--- covid_app/analytics/oc_daily_testing.py
+++ covid_app/analytics/oc_daily_testing.py
@@ -1,5 +1,5 @@
 """
-OC Daily Testing Test Analysis
+OC Daily Testing Analysis
 
 For info on OC HCA data sources, see:
 https://services2.arcgis.com/LORzk2hk9xzHouw9/ArcGIS/rest/services/occovid_pcr_csv/FeatureServer/0
@@ -7,21 +7,32 @@ https://services2.arcgis.com/LORzk2hk9xzHouw9/ArcGIS/rest/services/occovid_pcr_c
 from os.path import join as path_join
 from functools import cached_property
 import csv
-from datetime import datetime, date, timedelta
+from datetime import date, timedelta
 from calendar import monthrange, month_name
 
 from config.app import DATA_ROOT
+from covid_app.extracts.oc_hca.daily_archive_extract import OcDailyArchiveExtract
 
 
 OC_DATA_PATH = path_join(DATA_ROOT, 'oc')
 OC_ANALYTICS_DATA_PATH = path_join(OC_DATA_PATH, 'analytics')
-ANALYTICS_FILE_NAME_F = 'oc-test-analysis-{}-{}.csv'
+ANALYTICS_FILE_NAME_F = 'oc-daily-test-analysis-{}.csv'
 
-CSV_HEADER = ['Date', '+1d', '+2d', '+3d', '+4d', '+5d', '+6d', '+7d', '+8-14d',
-              '+15d+', 'TOTAL (as of {})']
+CSV_HEADER = [
+    'Date',
+    'Daily Test Repo',
+    'Daily New Cases',
+    'Repo Case Pos Rate',
+    'Daily Total Spec',
+    'Daily Pos Spec',
+    'Spec Test Pos Rate',
+    'Avg Spec Delay',       # Days
+    'Spec Delay Std Dev',   # Days
+    'Oldest Spec Updated'
+]
 
 
-class OcMonthlyTestAnalysis:
+class OcDailyTestingAnalysis:
     #
     # Properties
     #
@@ -43,60 +54,15 @@ class OcMonthlyTestAnalysis:
 
         return month_dates
 
-    @property
-    def extract_dates(self):
-        """Returns dates to pull extracts spanning from beginning of month requested
-        to yesterday.
-
-        Tests are reported next day, so we actually want to offset a day to go from
-        second day of month to yesterday.
-        """
-        extract_dates = []
-        num_days = (self.yesterday - self.start_date).days
-
-        for n in range(1, num_days+1):
-            dated = self.start_date + timedelta(days=n)
-            extract_dates.append(dated)
-
-        return extract_dates
-
     @cached_property
     def daily_extracts(self):
         dated_extracts = {}
 
-        for extract_date in self.extract_dates:
-            extract = OcDailyDataExtract(extract_date)
+        for extract_date in self.dates:
+            extract = OcDailyArchiveExtract(extract_date)
             dated_extracts[extract_date] = extract
 
         return dated_extracts
-
-    @cached_property
-    def total_tests_time_series(self):
-        time_series = {}
-        for dated in self.dates:
-            time_series[dated] = self.extract_total_tests_time_series_for_date(dated)
-        return time_series
-
-    @cached_property
-    def new_tests_time_series(self):
-        time_series = {}
-        for dated in self.dates:
-            time_series[dated] = self.extract_new_tests_time_series_for_date(dated)
-        return time_series
-
-    @cached_property
-    def total_positives_time_series(self):
-        time_series = {}
-        for dated in self.dates:
-            time_series[dated] = self.extract_total_positives_time_series_for_date(dated)
-        return time_series
-
-    @cached_property
-    def new_positives_time_series(self):
-        time_series = {}
-        for dated in self.dates:
-            time_series[dated] = self.extract_new_positives_time_series_for_date(dated)
-        return time_series
 
     @property
     def days_in_month(self):
@@ -111,56 +77,6 @@ class OcMonthlyTestAnalysis:
     def month_name(self):
         return month_name[self.month]
 
-    @property
-    def average_test_delay(self):
-        test_delay_days = []
-        tests = []
-
-        for dated in self.dates:
-            series = self.new_tests_time_series.get(dated)
-            if not series:
-                continue
-
-            for n, count in enumerate(series):
-                delay = n + 1
-                if count > 0:
-                    tests.append(count)
-                    test_delay_days.append(count * delay)
-
-        return sum(test_delay_days) / sum(tests)
-
-    @property
-    def average_positive_test_delay(self):
-        test_delay_days = []
-        tests = []
-
-        for dated in self.dates:
-            series = self.new_positives_time_series.get(dated)
-            if not series:
-                continue
-
-            for n, count in enumerate(series):
-                delay = n + 1
-                if count > 0:
-                    tests.append(count)
-                    test_delay_days.append(count * delay)
-
-        return sum(test_delay_days) / sum(tests)
-
-    @property
-    def longest_test_delay(self):
-        max_delay = 0
-        test_date = None
-
-        for dated, series in self.new_tests_time_series.items():
-            delay = len(self.truncate_time_series(series))
-
-            if delay > max_delay:
-                test_date = dated
-                max_delay = delay
-
-        return (test_date, max_delay)
-
     #
     # Instance Method
     #
@@ -169,146 +85,33 @@ class OcMonthlyTestAnalysis:
         self.month = month
 
     def to_csv(self):
-        tests_csv = self.series_to_csv('tests', self.new_tests_time_series)
-        positives_csv = self.series_to_csv('positives', self.new_positives_time_series)
-        return [tests_csv, positives_csv]
-
-    #
-    # Private
-    #
-    def series_to_csv(self, name, series):
         year_month = '{}{:02}'.format(self.year, self.month)
-        file_name = ANALYTICS_FILE_NAME_F.format(name, year_month)
+        file_name = ANALYTICS_FILE_NAME_F.format(year_month)
         csv_path = path_join(OC_ANALYTICS_DATA_PATH, file_name)
-
-        # Add data to last column
-        CSV_HEADER[-1] = CSV_HEADER[-1].format(self.yesterday)
 
         with open(csv_path, 'w', newline='') as f:
             writer = csv.writer(f)
             writer.writerow(CSV_HEADER)
 
             for dated in reversed(self.dates):
-                writer.writerow(self.series_data_to_csv_row(series, dated))
+                writer.writerow(self.extract_data_to_csv_row(dated))
 
         return csv_path
 
-    def series_data_to_csv_row(self, series, dated):
-        dated_dataset = series[dated]
-        plus_1d = self.get_series_value_by_day_offset(dated_dataset, 1)
-        plus_2d = self.get_series_value_by_day_offset(dated_dataset, 2)
-        plus_3d = self.get_series_value_by_day_offset(dated_dataset, 3)
-        plus_4d = self.get_series_value_by_day_offset(dated_dataset, 4)
-        plus_5d = self.get_series_value_by_day_offset(dated_dataset, 5)
-        plus_6d = self.get_series_value_by_day_offset(dated_dataset, 6)
-        plus_7d = self.get_series_value_by_day_offset(dated_dataset, 7)
-        plus_8_to_14d = self.get_series_value_by_day_offset_range(dated_dataset, 8, 14)
-        plus_15d_and_more = self.get_series_value_by_day_offset_range(dated_dataset, 15, -1)
-        total = sum(dated_dataset)
-
+    #
+    # Private
+    #
+    def extract_data_to_csv_row(self, dated):
+        extract = self.daily_extracts[dated]
         return [
             dated,
-            plus_1d,
-            plus_2d,
-            plus_3d,
-            plus_4d,
-            plus_5d,
-            plus_6d,
-            plus_7d,
-            plus_8_to_14d,
-            plus_15d_and_more,
-            total
+            extract.reported_new_tests,
+            extract.reported_new_cases_for_yesterday,
+            extract.reported_case_positive_rate,
+            extract.reported_total_admin_tests,
+            extract.reported_total_positive_tests,
+            extract.reported_test_positive_rate,
+            extract.average_test_delay,
+            extract.test_delay_stdev,
+            extract.oldest_updated_admin_test
         ]
-
-    def get_series_value_by_day_offset(self, series, day):
-        index = day - 1
-        if len(series) >= day:
-            return series[index]
-        else:
-            return None
-
-    def get_series_value_by_day_offset_range(self, series, start, end):
-        index = start - 1
-        if len(series) >= start:
-            subset = series[index:end]
-            return sum(subset)
-        else:
-            return None
-
-    def extract_total_tests_time_series_for_date(self, dated):
-        """Extract all total tests values ranging from dated to last daily extract.
-        """
-        series = []
-        num_days = (self.yesterday - dated).days
-
-        for n in range(num_days):
-            # Remember offset since tests are reported next day
-            next_day = n + 1
-            extract_date = dated + timedelta(next_day)
-            extract = self.daily_extracts[extract_date]
-            total_tests = extract.admin_tests.get(dated)
-            series.append(int(total_tests))
-
-        return series
-
-    def extract_new_tests_time_series_for_date(self, dated):
-        series = []
-        total_tests_series = self.total_tests_time_series.get(dated)
-
-        for n, total_tests in enumerate(total_tests_series):
-            if n == 0:
-                series.append(total_tests)
-                continue
-
-            prev = total_tests_series[n-1]
-            new_tests = total_tests - prev
-            series.append(int(new_tests))
-
-        return series
-
-    def extract_total_positives_time_series_for_date(self, dated):
-        series = []
-        num_days = (self.yesterday - dated).days
-
-        for n in range(num_days):
-            # Remember offset since tests are reported next day
-            next_day = n + 1
-            extract_date = dated + timedelta(next_day)
-            extract = self.daily_extracts[extract_date]
-            total_tests = extract.positive_tests.get(dated)
-            series.append(int(total_tests))
-
-        return series
-
-    def extract_new_positives_time_series_for_date(self, dated):
-        series = []
-        totals_series = self.total_positives_time_series.get(dated)
-
-        for n, totals in enumerate(totals_series):
-            if n == 0:
-                series.append(totals)
-                continue
-
-            prev = totals_series[n-1]
-            new_positives = totals - prev
-            series.append(int(new_positives))
-
-        return series
-
-    def truncate_time_series(self, series):
-        """Truncates time series at last positive value.
-        """
-        tail = 0
-
-        for value in reversed(series):
-            if value <= 0:
-                tail -= 1
-            else:
-                break
-
-        if tail == 0:
-            truncated_series = series
-        else:
-            truncated_series = series[0:tail]
-
-        return truncated_series

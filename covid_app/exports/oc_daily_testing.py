90cfd0d74b4e7e77d06c07841556b81e0be8d53d
klenwell <klenwell@gmail.com>
2020-10-22 22:38:43 -0700

    Adds CSV headers for OC Daily Testing export. Starts to fill in row data.

--- covid_app/exports/oc_daily_testing.py
+++ covid_app/exports/oc_daily_testing.py
@@ -20,6 +20,29 @@ START_DATE = '2020-08-03'
 
 CSV_HEADER = [
     'Date',
+    '7d Avg Test Delay (Days)',
+    'Effective Reporting Date',
+    'Tests Reported (Repo)',
+    'Test Specs Reported',
+    'Pos Specs Reported',
+    'New Cases Reported',
+    'Test Specs Dated Today',
+    'Pos Specs Dated Today',
+    'Oldest Test Reported',
+    '7d Avg Test Specs',
+    '7d Avg Pos Specs',
+    '7d Avg Spec Pos Rate',
+    '7d Avg Case Rate',
+    'Tests Delayed 1-2d',
+    'Tests Delayed 3-4d',
+    'Tests Delayed 5-7d',
+    'Tests Delayed 8-14d',
+    'Tests Delayed 15+d',
+    'Pos Tests Delayed 1-2d',
+    'Pos Tests Delayed 3-4d',
+    'Pos Tests Delayed 5-7d',
+    'Pos Tests Delayed 8-14d',
+    'Pos Tests Delayed 15+d'
 ]
 
 
@@ -38,7 +61,7 @@ class OcDailyTestsExport:
     @cached_property
     def dates(self):
         dates = []
-        of_days = (self.end_date - self.start_date).days
+        of_days = (self.end_date - self.start_date).days + 1
 
         for n in range(of_days):
             dated = self.start_date + timedelta(days=n)
@@ -55,6 +78,10 @@ class OcDailyTestsExport:
             dated_extracts[extract_date] = extract
 
         return dated_extracts
+
+    @cached_property
+    def most_recent_daily_extract(self):
+        return self.daily_extracts[self.end_date]
 
     @cached_property
     def csv_path(self):
@@ -83,8 +110,35 @@ class OcDailyTestsExport:
     #
     # Private
     #
-    def extract_data_to_csv_row(self, dated):
-        extract = self.daily_extracts[dated]
+    def extract_data_to_csv_row(self, extract_date):
+        extract = self.daily_extracts[extract_date]
+
+        # Remember: the extract will be reporting values from the day previous.
+        reporting_date = extract_date - timedelta(days=1)
+
         return [
-            dated
+            reporting_date,
+            '7d Avg Test Delay (Days)',
+            'Effective Reporting Date',
+            extract.reported_new_tests,
+            extract.reported_total_admin_tests,
+            extract.reported_total_positive_tests,
+            extract.reported_new_cases_for_yesterday,
+            self.most_recent_daily_extract.admin_tests.get(reporting_date, 'N/A'),
+            self.most_recent_daily_extract.positive_tests.get(reporting_date, 'N/A'),
+            extract.oldest_updated_admin_test,
+            '7d Avg Test Specs',
+            '7d Avg Pos Specs',
+            '7d Avg Spec Pos Rate',
+            '7d Avg Case Rate',
+            'Tests Delayed 1-2d',
+            'Tests Delayed 3-4d',
+            'Tests Delayed 5-7d',
+            'Tests Delayed 8-14d',
+            'Tests Delayed 15+d',
+            'Pos Tests Delayed 1-2d',
+            'Pos Tests Delayed 3-4d',
+            'Pos Tests Delayed 5-7d',
+            'Pos Tests Delayed 8-14d',
+            'Pos Tests Delayed 15+d'
         ]

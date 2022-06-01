4d1b6df0b7cc920e5c27ac0d318d9c2639e5c670
klenwell <klenwell@gmail.com>
2022-05-31 22:06:15 -0700

    Adds new wave models.

---
+++ covid_app/models/oc/epidemic_wave.py
@@ -0,0 +1,253 @@
+"""
+EpidemicWave
+
+data_series arg should be a map of dates to values.
+"""
+from functools import cached_property
+from datetime import datetime, timedelta
+from math import floor, inf
+from pprint import pformat
+from itertools import islice
+from collections import deque
+
+from config.app import WAVE_ANALYSIS_CONFIG
+from covid_app.models.oc.wave_phase import WavePhase
+from covid_app.models.oc.phase_window import PhaseWindow
+
+
+class PhaseSmoothingError(Exception):
+    def __init__(self, phases):
+        message = 'Unable to smooth jagged phases:\n {}'.format(pformat(phases))
+        super().__init__(message)
+        self.phases = phases
+
+
+def sliding_window(iterable, n):
+    """Source: https://docs.python.org/3/library/itertools.html#itertools-recipes
+    sliding_window('ABCDEFG', 4) -> ABCD BCDE CDEF DEFG
+    """
+    it = iter(iterable)
+    window = deque(islice(it, n), maxlen=n)
+    if len(window) == n:
+        yield tuple(window)
+    for x in it:
+        window.append(x)
+        yield tuple(window)
+
+
+class EpidemicWave:
+    def __init__(self, data_series, **opts):
+        self.data_series = data_series
+        self.debug = opts.get('debug', False)
+        self.window_size = opts.get('window_size', WAVE_ANALYSIS_CONFIG['window_size'])
+        self.flat_slope_threshold = opts.get('flat_slope_threshold',
+                                             WAVE_ANALYSIS_CONFIG['flat_slope_threshold'])
+        self.min_phase_size = opts.get('min_phase_size',
+                                       WAVE_ANALYSIS_CONFIG['min_phase_size'])
+
+    #
+    # Properties
+    #
+    @cached_property
+    def dates(self):
+        return sorted(self.data_series.keys())
+
+    @cached_property
+    def smoothed_phases(self):
+        return self.smooth_phases(self.phases)
+
+    @cached_property
+    def phases(self):
+        phases = []
+        prev_window = self.windows[0]
+        open_phase = WavePhase(prev_window)
+
+        for window in self.windows[1:]:
+            trend_change = window.trend != prev_window.trend
+
+            if trend_change:
+                open_phase.end(window)
+                phases.append(open_phase)
+                open_phase = WavePhase(window)
+
+            prev_window = window
+
+        if open_phase.start_window != window:
+            open_phase.end(window)
+            phases.append(open_phase)
+
+        return phases
+
+    @cached_property
+    def windows(self):
+        windows = []
+        half_window_len = floor(self.window_size / 2)
+
+        for dated in self.dates:
+            window_rates = []
+
+            for n in range(-half_window_len, half_window_len+1):
+                rate_date = dated + timedelta(days=n)
+                rate = self.data_series.get(rate_date)
+                if rate:
+                    window_rates.append(rate)
+
+            if len(window_rates) == self.window_size:
+                window = PhaseWindow(dated, window_rates)
+                windows.append(window)
+
+        return windows
+
+    @cached_property
+    def std_devs(self):
+        dated_values = {}
+
+        for dated in self.dates[1:]:
+            yesterday = dated - timedelta(days=1)
+            window = self.windows.get(yesterday)
+
+            if not window:
+                continue
+
+            diff = self.avg_positive_rates[dated] - window['mean']
+            dev = diff / window['stdev']
+            dated_values[dated] = dev
+
+        return dated_values
+
+    #
+    # Methods
+    #
+    def generate_waves(self):
+        waves = []
+        return waves
+
+    def smooth_phases(self, phases):
+        def log(msg, n, seq): self.debug and print(msg, n, len(seq), "\n", pformat(seq))
+        series_is_jagged = self.phase_series_is_jagged(phases)
+        n = 0
+
+        while series_is_jagged:
+            n += 1
+            pre_merge_count = len(phases)
+
+            phases = self.merge_running_trends(phases)
+            log('post running merges', n, phases)
+            phases = self.merge_micro_phases(phases)
+            log('post micro merges', n, phases)
+
+            series_is_jagged = self.phase_series_is_jagged(phases)
+            if series_is_jagged and len(phases) == pre_merge_count:
+                raise PhaseSmoothingError(phases)
+
+        return phases
+
+    def phase_series_is_jagged(self, phases):
+        """These things are considered unjagged or unsmooth:
+        - any micro phases
+        - any consecutive phases with same trend
+        """
+        prev_phase = None
+        for phase in phases[:-1]:
+            if phase.is_micro():
+                return True
+            if prev_phase and prev_phase.trend == phase.trend:
+                return True
+            prev_phase = phase
+        return False
+
+    def merge_running_trends(self, phases):
+        merged_phases = []
+        prev_phase = phases[0]
+
+        # TODO: remove this if you ever abstract this out. This is OC-specific.
+        seesaw_2020_period_start = datetime(2020, 3, 31).date()
+        seesaw_2020_period_end = datetime(2020, 6, 11).date()
+
+        for phase in phases[1:]:
+            # This is a dirty hack to deal with irregular data early in pandemic
+            if phase.started_on > seesaw_2020_period_start and \
+                phase.started_on < seesaw_2020_period_end and \
+                    prev_phase.trending == 'falling':
+                phase = phase.merge(prev_phase)
+            elif phase.trend == prev_phase.trend:
+                phase = phase.merge(prev_phase)
+            else:
+                merged_phases.append(prev_phase)
+            prev_phase = phase
+
+        # Don't forget last phase
+        merged_phases.append(phase)
+        return merged_phases
+
+    def merge_micro_phases(self, phases):
+        merged_phases = []
+        prev_phase = None
+        merge_on_next_loop = False
+
+        for phase, next_phase in sliding_window(phases, 2):
+            # print(prev_phase, phase, next_phase)
+            merge_with_prev = False     # reset flag each loop
+
+            # Merge phases as flagged in previous loop.
+            if merge_on_next_loop:
+                merge_on_next_loop = False
+                phase = phase.merge(prev_phase)
+
+                # If prev phase was added to smooth, pop and replace with merged one
+                if prev_phase in merged_phases:
+                    merged_phases.pop()
+
+            # phase no longer micro, continue to next one
+            if not phase.is_micro():
+                merged_phases.append(phase)
+                prev_phase = phase
+                continue
+
+            # Now figure out what to do with micro phases.
+            prev_phase_slope = prev_phase.kslope if prev_phase is not None else inf
+            prev_phase_flatter = abs(prev_phase_slope) < abs(next_phase.kslope)
+            prev_phase_higher = prev_phase and prev_phase.end_value > next_phase.end_value
+
+            # If no previous, just merge with next phase
+            if not prev_phase:
+                merge_on_next_loop = True
+            # If flat, merge with whichever neighbor is flatter
+            elif phase.trending == 'flat':
+                if prev_phase_flatter:
+                    merge_with_prev = True
+                else:
+                    merge_on_next_loop = True
+            # If next phase is lower then previous, merge with next since this should extend trend
+            elif phase.trending == 'rising':
+                if prev_phase.trending == 'rising':
+                    merge_with_prev = True
+                # FIXME? Comment above and this line seem inconsistent.
+                elif next_phase.end_value < prev_phase.end_value:
+                    merge_on_next_loop = True
+                else:
+                    merge_with_prev = True
+            # Same logic as rising reversed
+            else:  # phase.trending == 'falling'
+                if prev_phase.trending == 'falling':
+                    merge_with_prev = True
+                elif next_phase.end_value < prev_phase.end_value:
+                    merge_on_next_loop = True
+                else:
+                    merge_with_prev = True
+
+            # Merge with previous if flagged above
+            if merge_with_prev:
+                phase = phase.merge(prev_phase)
+                if prev_phase in merged_phases:
+                    merged_phases.pop()
+
+            if not merge_on_next_loop:
+                merged_phases.append(phase)
+
+            # on to next loop
+            prev_phase = phase
+
+        # Don't forget last phase
+        merged_phases.append(phases[-1])
+        return merged_phases
---
+++ covid_app/models/oc/phase_window.py
@@ -0,0 +1,54 @@
+import statistics
+from math import floor
+
+from config.app import WAVE_ANALYSIS_CONFIG
+
+
+class PhaseWindow:
+    def __init__(self, dated, values, **opts):
+        self.date = dated
+        self.values = values
+        self.value = values[floor(len(values) / 2)]
+        self.flat_slope_threshold = opts.get('flat_slope_threshold',
+                                             WAVE_ANALYSIS_CONFIG['flat_slope_threshold'])
+
+    @property
+    def value_change(self):
+        return self.end_value - self.start_value
+
+    @property
+    def slope(self):
+        return self.value_change / len(self.values)
+
+    @property
+    def kslope(self):
+        return self.slope * 100
+
+    @property
+    def stdev(self):
+        return statistics.stdev(self.values)
+
+    @property
+    def mean(self):
+        return statistics.mean(self.values)
+
+    @property
+    def trend(self):
+        if self.kslope > self.flat_slope_threshold:
+            return 1
+        elif self.kslope < -self.flat_slope_threshold:
+            return -1
+        else:
+            return 0
+
+    @property
+    def start_value(self):
+        return self.values[0]
+
+    @property
+    def end_value(self):
+        return self.values[-1]
+
+    def __repr__(self):
+        f = '<PhaseWindow middate={} days={} kslope={:.1f} stdev={:.3f} trend={}>'
+        return f.format(self.date, len(self.values), self.kslope, self.stdev, self.trend)

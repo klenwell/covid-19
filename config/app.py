9184541cf6d5cf6d8f1f67fabbf2c5569dea2046
klenwell <klenwell@gmail.com>
2022-05-31 22:09:38 -0700

    Adds wave analysis defaults to app config file.

--- config/app.py
+++ config/app.py
@@ -5,3 +5,15 @@ APP_ROOT = path_join(PROJECT_ROOT, 'covid_app')
 
 DATA_ROOT = path_join(PROJECT_ROOT, 'data')
 GH_PAGES_ROOT = path_join(PROJECT_ROOT, 'docs')
+
+# OC Wave Analysis
+WAVE_ANALYSIS_CONFIG = {
+    'window_size': 5,
+
+    # Slope value distinguishing plateaus from rise/falls
+    'flat_slope_threshold': 5,
+
+    # This is the minimum size a phase needs to be (in days) not be flagged as micro
+    # and merged.
+    'min_phase_size': 14
+}

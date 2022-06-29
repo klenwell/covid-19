5aa64d19290670d0a55be5dea4890f16620a1feb
klenwell <klenwell@gmail.com>
2022-06-28 21:11:35 -0700

    Weekly OC testing update.

--- docs/js/oc-phases-model.js
+++ docs/js/oc-phases-model.js
@@ -56,9 +56,8 @@ class OcPhasesModel {
       11: 'Autumn Lull',
       12: `Holiday ${link('Omicron')} Surge`,
       13: 'Post-Omicron Drop',
-      14: 'Early Spring Lull',
-      15: `Late Spring ${link('Omicron 2')} Surge`,
-      16: `Post-Omicron-2 Drop?`
+      14: 'Mini Spring Lull',
+      15: `Early Summer ${link('Omicron 2')} Surge<br />(Ongoing)`,
     }
   }
 
